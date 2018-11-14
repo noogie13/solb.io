c05fc5a78b8068d26450a589589efc7292df7fa7
Solomon Bloch <solomonbloch@gmail.com>
2018-11-14 01:18:17 -0500

    Added using http-kit instead of using the silly lein ring server-headless.

--- src/solb/handler.clj
+++ src/solb/handler.clj
@@ -1,5 +1,7 @@
 (ns solb.handler
   (:require [compojure.core :refer :all]
+            [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]
             [compojure.route :as route]
             [ring.util.response :as resp]
             [templates.layout :as layout]
@@ -7,8 +9,18 @@
 
 (defroutes app-routes
   (GET "/" [] (layout/homepage))
-  (GET "/googlee5d78e21ce009a63.html" [] (route/files "googlee5d78e21ce009a63.html"))
+  (GET "/test" [] (str (t/now)))
+  (route/files "/static/")
   (route/not-found "404 : hi from sol, where ya going? (text me the answer @666-666-6666)"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (@server :timeout 5)
+  (reset! server nil))
+
+(defn -main [& args]
+  (reset! server (run-server #'app {:port 3000})))
