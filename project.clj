986c09c3d201c5630f5b05155ade7bd990844684
Solomon Bloch <solomonbloch@gmail.com>
2018-11-14 01:17:30 -0500

    Added: main namespace, cider-nrepl, and http-kit.

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [hiccup "1.0.5"]
+                 [http-kit "2.2.0"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.4"]]
+  :main solb.handler
+  :plugins [[lein-ring "0.12.4"]
+            [cider/cider-nrepl "0.18.0"]]
   :ring {:handler solb.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
