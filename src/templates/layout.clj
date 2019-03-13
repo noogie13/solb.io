635019ebaf5002200f30df3d23730122a0bd064d
Solomon Bloch <solomonbloch@gmail.com>
2019-03-13 21:45:45 +0900

    added blog stuff here, also lots of styling

--- src/templates/layout.clj
+++ src/templates/layout.clj
@@ -2,18 +2,26 @@
   (:require
    [hiccup.core :refer :all]
    [hiccup.element :as elem]
-   [hiccup.page :refer [include-css html5]]))
+   [clojure.string :as str]
+   [clj-time.format :as f]
+   [clj-time.coerce :as tc]
+   [templates.db :as db]
+   [honeysql.core :as sql]
+   [honeysql.helpers :as helpers :refer :all]
+   [clojure.java.jdbc :as jdbc]
+   [hiccup.page :refer [include-css html5]]
+   [templates.blog :as blog]))
 
 (defn navbar
   []
   (html [:div.navbarcontain
-         [:div.navbar
-          [:a.left {:href "/"}
-           "home"]
-          [:a.left {:href "/blog"}
-           "blog"]
-          [:a.right {:href "#contact"}
-           "contact"]]]))
+         [:div.navbar {:style "font-family: 'Jacques Francois';"}
+          ;; [:a.left {:href "/"} "Home"]
+          [:a.right {:href "/blog"} "Blog"]
+          [:a.right {:href "/aboutme"} "About Me"]
+          [:div.navbarname [:a {:href "/"}
+                            "Solomon Bloch"]]
+          ]]))
 
 
 (defn homepage
@@ -26,70 +34,194 @@
              :content "width=device-width, initial-scale=1.0"}]
      [:title "solB"]]
     [:body
-     [:header
-      (navbar)
-      [:h1 "S"
-       [:span {:style "font-size: 26px;"}
-        "OLOMON"] " BLOCH"]]
+     [:header (navbar)]
+     ;; [:h1 "S"
+     ;;  [:span {:style "font-size: 26px;"}
+     ;;   "OLOMON"] " BLOCH"]
      ;; [:div.hobbies
      ;;  [:p "hobby typist"]]
-      [:img.face {:src "images/me.jpg"}]
-      [:div.social
-       (elem/link-to {:target "_blank"} "https://www.linkedin.com/in/solomon-bloch-151309167/"
-                     [:img.icons {:src "images/social/256/linkedin.png"}])
-       (elem/link-to {:target "_blank"} "https://www.instagram.com/noogietheguru"
-                     [:img.icons {:src "images/social/256/insta.png"}])
-       (elem/mail-to "mailto:solomonbloch@gmail.com"
-                     [:img.icons {:src "images/social/256/google+.png"}])]
-      [:br]
-      [:br]
-      [:br]
-      [:div.strengths
-       [:div.strengthflex
-        [:div.abouticon {:style "background-image:url('images/backgrounds/mars1.jpg')"}
-         [:div.darken
-          [:div.break]
-          [:h2 {:style "font-family: Hack, monospace;"}
-           "PROGRAMMING"]
-          [:div.terminal
-           "sol@"
-           [:span {:style "color:aqua"}
-            "sol-pc"] "-&gt; pwd"
-           [:br]
-           [:br] "/home/sol/Documents/"
-           [:br]
-           [:br] "sol@"
-           [:span {:style "color:aqua"}
-            "sol-pc"] "-&gt; Love for programming, picks up new languages fast. Special place for functional programming."]]]
-        [:div.abouticon
-               {:style "background-image:url('images/backgrounds/mars2.jpg'); font-family: 'Computer Modern', serif;"}
-         [:div.darken
-          [:div.break]
-          [:h2 {:style "font-family:'Computer Modern', serif; font-weight: normal;"}
-           "MATHEMATICS"]
-          [:div.math-text
-           "Studying applied mathematics, enjoys diving into questions of not just how do we solve a problem, but how can we solve it " [:i "optimally."]]]]
-        [:div.abouticon
-               {:style "background-image:url('images/backgrounds/mars3.jpg')"}
-         [:div.darken
-          [:div.break]
-          [:h2 "DATA ANALYSIS"]
-          [:div.math-text
-                 {:style "font-family: Alcubierre;"}
-           " "
-           [:table.home
-            [:tr.home [:th.home "Last Name"] [:th.home "First Name"] [:th.home "Zip"]]
-            [:tr.home [:th.home "Hathaway"] [:th.home "Donny"] [:th.home "60007"]]
-            [:tr.home [:th.home "Gaye"] [:th.home "Marvin"] [:th.home "20001"]]]
-           [:br]
-           "But what does it mean? Analyzing data and searching for actionables is a proficiency and interest."]]]]]
+     [:div.first
+      [:div.face
+       [:img.face {:src "images/me.jpg"}]
+       [:div.social
+        (elem/link-to {:target "_blank"} "https://www.linkedin.com/in/solomon-bloch-151309167/"
+                      [:img.icons {:src "images/social/256/linkedin.png"}])
+        (elem/link-to {:target "_blank"} "https://www.instagram.com/noogietheguru"
+                      [:img.icons {:src "images/social/256/insta.png"}])
+        (elem/mail-to "mailto:solomonbloch@gmail.com"
+                      [:img.icons {:src "images/social/256/google+.png"}])]]
+      [:div.bloglist
+       (for [i (reverse (blog/live-posts))]
+         [:div.entry
+          [:a.entry {:href (str "/blog/" (:link i))}
+           (:title i)]
+          [:div.datetagsflex
+           [:div.tags
+            (for [tag (str/split (:tags i) #" ")]
+              (elem/link-to (str "/blog/tags/" tag)
+                            (str ":" tag)))]
+           [:p.date (f/unparse (f/formatters :date)
+                               (tc/from-sql-time (:date i)))]]
+          [:p.forward (:forward i)]])
+       ;; [:div.more
+       ;;  [:a {:href "/blog"}
+       ;;   [:div.morewords "↓ see more posts ↓"]]]
+       ]]
+     ]]))
+(defn aboutme
+  []
+  (html5
+   (include-css "styles/style.css")
+   [:html
+    [:head
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:title "solB"]]
+    [:body
+     [:header (navbar)]
+     [:div.aboutme
+      [:div.strengthflex
+       [:div.abouticon {:style "background-image:url('images/backgrounds/mars1.jpg')"}
+        [:div.darken
+         [:div.break]
+         [:h2 {:style "font-family: Hack, monospace;"}
+          "PROGRAMMING"]
+         [:div.terminal
+          "sol@"
+          [:span {:style "color:aqua"}
+           "sol-pc"] "-&gt; pwd"
+          [:br] [:br] "/home/sol/Documents/" [:br] [:br] "sol@"
+          [:span {:style "color:aqua"}
+           "sol-pc"] "-&gt; Love for programming, picks up new languages fast. Special place for functional programming."]]]
+       [:div.abouticon
+        {:style "background-image:url('images/backgrounds/mars2.jpg'); font-family: 'Computer Modern', serif;"}
+        [:div.darken
+         [:div.break]
+         [:h2 {:style "font-family:'Computer Modern', serif; font-weight: normal;"}
+          "MATHEMATICS"]
+         [:div.math-text
+          "Studying applied mathematics, enjoys diving into questions of not just how do we solve a problem, but how can we solve it " [:i "optimally."]]]]
+       [:div.abouticon
+        {:style "background-image:url('images/backgrounds/mars3.jpg')"}
+        [:div.darken
+         [:div.break]
+         [:h2 "DATA ANALYSIS"]
+         [:div.math-text
+          {:style "font-family: Raleway;"}
+          " "
+          [:table.home
+           [:tr.home [:th.home "Last Name"] [:th.home "First Name"] [:th.home "Zip"]]
+           [:tr.home [:th.home "Hathaway"] [:th.home "Donny"] [:th.home "60007"]]
+           [:tr.home [:th.home "Gaye"] [:th.home "Marvin"] [:th.home "20001"]]]
+          [:br]
+          "But what does it mean? Analyzing data and searching for actionables is a proficiency and interest."]]]]
       [:div.experience
+       [:div.break {:style "background-color: #2f363a;"}]
        [:h2 "EXPERIENCE"]
        [:div.experienceflex
-        [:img {:src "images/asseen/fh.png"}]]]
+        [:img {:src "images/asseen/fh.png" :style "width: 200px; height: 200px;"}]]]
       [:div.asseen
        [:div.colorlayer
         [:div.break]
         [:h2 "AS SEEN"]
         (elem/link-to {:target "_blank"}
-                      "http://dailyorange.com/2016/11/super-smash-bros-builds-community-across-central-new-york/")]]]]))
+                      "http://dailyorange.com/2016/11/super-smash-bros-builds-community-across-central-new-york/")]]]]]))
+
+
+(defn blog-post-html
+  "takes a list of blog entries and turns it into html"
+  [post-list]
+  (html5
+   (include-css "/styles/style.css")
+   [:html
+    [:head
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:title "solB"]]
+    [:body
+     [:header
+      (navbar)]
+     [:div.blogonly
+      (for [i (reverse post-list)]
+        [:div.entry
+         [:a.entry {:href (str "/blog/" (:link i))}
+          (:title i)]
+         [:div.datetagsflex
+          [:div.tags
+           (for [tag (str/split (:tags i) #" ")]
+             (elem/link-to (str "/blog/tags/" tag)
+                           (str ":" tag)))]
+          [:div.date (f/unparse (f/formatters :date)
+                                (tc/from-sql-time (:date i)))]]
+         [:p.forward (:forward i)]
+         ])]]]))
+
+
+(defn blog-homepage
+  []
+  (blog-post-html (blog/live-posts)))
+
+(defn tag-page
+  "page that shows posts with the same tag"
+  [tag]
+  (blog-post-html (blog/like-tag tag)))
+
+
+
+(defn htmlitize
+  "make a post html (fill up title content etc)"
+  [entry-title]
+  (let [entry (first (jdbc/query db/pg-db (-> (select :*)
+                                              (from :posts)
+                                              (where [:= :link
+                                                      entry-title])
+                                              sql/format)))]
+    (html5
+     (include-css "/styles/style.css")
+     [:html
+      [:head
+       [:meta {:name "viewport"
+               :content "width=device-width, initial-scale=1.0"}]
+       [:title "solB"]]
+      [:body
+       [:header (navbar)]
+       [:div.blog
+        [:h2 (:title entry)]
+        [:div.datetagsflex
+         [:div.tags
+          (for [tag (str/split (:tags entry) #" ")]
+            (elem/link-to (str "/blog/tags/" tag)
+                          (str ":" tag)))]
+         [:div.date (f/unparse (f/formatters :date)
+                             (tc/from-sql-time (:date entry)))]]
+        [:p (:content entry)]]]])))
+
+;; (defn htmltest
+;;   "make a post html (fill up title content etc)"
+;;   []
+;;   (let [entry {:title "clojure blog 1: hiccup and ring",
+;;                :tags "these are tags",
+;;                :date (tc/to-sql-time (t/now)),
+;;                :content (content-format "Not that there aren't a million tutorials of this already, but let's examine some of the basics of Clojure's beautiful HTML ```something```")}]
+;;     (html5
+;;      (include-css "/styles/style.css")
+;;      [:html
+;;       [:head
+;;        [:meta {:name "viewport"
+;;                :content "width=device-width, initial-scale=1.0"}]
+;;        [:title "solB"]]
+;;       [:body
+;;        [:header
+;;         (navbar)
+;;         [:h1 "S"
+;;          [:span {:style "font-size: 26px;"}
+;;           "OL"] "B"]]
+;;        [:div.blog
+;;         [:h2 (:title entry)]
+;;         [:div.tags
+;;          (for [tag (str/split (:tags entry) #" ")]
+;;            (elem/link-to (str "/blog/tags/" tag)
+;;                          (str ":" tag)))]
+;;         [:p.date (f/unparse (f/formatters :date)
+;;                             (tc/from-sql-time (:date entry)))]
+;;         [:div.content (:content entry)]]]])))
