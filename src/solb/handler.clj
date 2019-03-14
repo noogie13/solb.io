450b64004bafbb33cbf9dca7bc1585ae4c7268bf
Solomon Bloch <solomonbloch@gmail.com>
2019-03-14 16:02:07 +0900

    editing.... ^_^

--- src/solb/handler.clj
+++ src/solb/handler.clj
@@ -2,31 +2,38 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [hiccup.page :refer [html5]]
-            [solb.users :as users]
+            [backend.users :as users]
             [org.httpkit.server :refer [run-server]]
             [ring.util.response :as resp]
             [ring.middleware.cookies :refer [wrap-cookies]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]
             [templates.layout :as layout]
-            [templates.blog :as blog]
             [templates.login :as login]
+            [templates.blog :as blog]
             [clojure.java.io :as io]))
+
 (defn print-request
   [request]
   (html5 request))
 
+(defn print-content
+  [req]
+  (html5  (req :params)))
+
 (defroutes app-routes
   (GET "/" [] (layout/homepage))
   (GET "/aboutme" [] (layout/aboutme))
-  (GET "/blog/:entry" [entry] (layout/htmlitize entry))
-  (GET "/blog" [] (layout/blog-homepage))
-  (GET "/blog/tags/:tag" [tag] (layout/tag-page tag))
+  (GET "/blog/:entry" [entry] (blog/htmlitize entry))
+  (GET "/blog/:entry/edit" [entry] (blog/htmlitize-edit! entry))
+  (POST "/editor" [] backend.blog/edit!)
+  (GET "/blog" [] (blog/blog-homepage))
+  (GET "/blog/tags/:tag" [tag] (blog/tag-page tag))
   (GET "/login" [] (login/login-page))
   (POST "/login" [] users/login-user)
   (GET "/tester" [] users/tester)
   (GET "/prntreq" [] print-request)
-  (GET "/admin" [] )
+  (GET "/admin" [:as req] (users/sol? req blog/admin))
   (route/not-found "where are we going? "))
 
 (def app
