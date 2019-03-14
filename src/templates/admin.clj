1593f929ee89df113431a2b3b5278f51724bad51
Solomon Bloch <solomonbloch@gmail.com>
2019-03-14 16:57:00 +0900

    moved files around

--- src/templates/admin.clj
+++
@@ -1,8 +0,0 @@
-(ns templates.admin
-  (:require
-   [clojure.java.io :as io]
-   [ring.util.anti-forgery :as anti]
-   [hiccup.form :as form]
-   [hiccup.core :refer :all]
-   [hiccup.page :refer [include-css html5]]))
-
