b2d66c3e83606ddf685693e1f77102ca825f0437
Solomon Bloch <solomonbloch@gmail.com>
2019-03-14 16:57:39 +0900

    mainly changing htmlizing-edit!

--- src/templates/blog.clj
+++ src/templates/blog.clj
@@ -1,69 +1,150 @@
 (ns templates.blog
-  (:require [clj-time.coerce :as tc]
-            [clj-time.core :as t]
-            [clj-time.format :as f]
-            [clojure.core :refer :all]
-            [clojure.java.jdbc :as jdbc]
-            [hiccup.element :as elem]
-            [hiccup.page :refer [html5 include-css include-js]]
-            [honeysql.core :as sql]
-            [honeysql.helpers :as helpers :refer :all]
-            [templates.db :as db]
-            [glow.core :as glow]
-            [clojure.string :as str]))
+  (:require
+   [hiccup.core :refer :all]
+   [hiccup.element :as elem]
+   [hiccup.form :as form]
+   [ring.util.anti-forgery :as anti]
+   [clojure.string :as str]
+   [clj-time.format :as f]
+   [clj-time.coerce :as tc]
+   [backend.db :as db]
+   [honeysql.core :as sql]
+   [honeysql.helpers :as helpers :refer :all]
+   [clojure.java.jdbc :as jdbc]
+   [hiccup.page :refer [include-css include-js html5]]
+   [backend.blog :as blog]
+   [templates.layout :as layout]))
 
-(defn content-format
-  [content]
-  (-> content
-      (str/replace #"(?is)```(.*)```"
-                   (str "<pre><code>" "$1" "</pre></code>"))
-      (str/replace #"`(.*)`"
-                   (str "<span class=\"tidbit\">" "$1" "</span>"))
-      (str )))
+(defn blog-post-html
+  "takes a list of blog entries and turns it into html"
+  [post-list]
+  (html5
+   (include-css "/styles/style.css")
+   [:html
+    [:head
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:title "solB"]]
+    [:body
+     [:header
+      (layout/navbar)]
+     [:div.blogonly
+      (for [i (reverse post-list)]
+        [:div.entry
+         [:a.entry {:href (str "/blog/" (:link i))}
+          (:title i)]
+         [:div.datetagsflex
+          [:div.tags
+           (for [tag (str/split (:tags i) #" ")]
+             (elem/link-to (str "/blog/tags/" tag)
+                           (str ":" tag)))]
+          [:div.date (f/unparse (f/formatters :date)
+                                (tc/from-sql-time (:date i)))]]
+         [:p.forward (:forward i)]])]]]))
 
 
-(defn make-draft!
-  "make a new post, set as a draft"
-  [& {:keys [title tags content forward]
-      :or {title "no title"
-           tags "no tags"
-           content "empty"
-           forward "no forward"}}]
-  (let [link (clojure.string/lower-case
-              (clojure.string/replace title " " "-"))
-        date (tc/to-sql-time (t/now))]
-    (jdbc/execute! db/pg-db (-> (insert-into :posts)
-                                (columns :title :status
-                                         :link :date
-                                         :tags :content
-                                         :forward)
-                                (values
-                                 [[title true
-                                   link date
-                                   tags content forward]])
-                                sql/format))))
-
-(defn drafts-posts
-  "pull all draft posts in a sequence"
+(defn blog-homepage
   []
-  (jdbc/query db/pg-db (-> (select :*)
-                           (from :posts)
-                           (where [:is :status false])
-                           sql/format)))
+  (blog-post-html (blog/live-posts)))
 
-(defn live-posts
-  "pull all live posts in a sequence"
-  []
-  (jdbc/query db/pg-db (-> (select :*)
-                           (from :posts)
-                           (where [:is :status true])
-                           sql/format)))
-
-(defn like-tag
-  "pull other posts with the tag ~tag"
+(defn tag-page
+  "page that shows posts with the same tag"
   [tag]
-  (jdbc/query db/pg-db (-> (select :*)
-                           (from :posts)
-                           (where ["tags::text like "
-                                   (str "%" tag "%")])
-                           sql/format)))
+  (blog-post-html (blog/like-tag tag)))
+
+(defn admin
+  []
+  (html5
+   (include-css "/styles/style.css")
+   [:html
+    [:head
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:title "solB"]]
+    [:body
+     [:header
+      (layout/navbar)]
+     [:div.blogonly
+      (for [i (reverse (blog/live-posts))]
+        [:div.entry
+         [:a.entry {:href (str "/blog/" (:link i))}
+          (:title i)]
+         [:div.datetagsflex
+          [:div.tags
+           (elem/link-to (str "/blog/" (:link i) "/edit")
+                         "::edit::")
+           (for [tag (str/split (:tags i) #" ")]
+             (elem/link-to (str "/blog/tags/" tag)
+                           (str ":" tag)))]
+          [:div.date (f/unparse (f/formatters :date)
+                                (tc/from-sql-time (:date i)))]]
+         [:p.forward (:forward i)]])]]]))
+
+(defn htmlitize
+  "make a post html (fill up title content etc)"
+  [entry-title]
+  (let [entry (first (jdbc/query db/pg-db (-> (select :*)
+                                              (from :posts)
+                                              (where [:= :link
+                                                      entry-title])
+                                              sql/format)))]
+    (html5
+     (include-css "/styles/style.css")
+     [:html
+      [:head
+       [:meta {:name "viewport"
+               :content "width=device-width, initial-scale=1.0"}]
+       [:title "solB"]]
+      [:body
+       [:header (layout/navbar)]
+       [:div.blog
+        [:h2 (:title entry)]
+        [:div.datetagsflex
+         [:div.tags
+          (for [tag (str/split (:tags entry) #" ")]
+            (elem/link-to (str "/blog/tags/" tag)
+                          (str ":" tag)))]
+         [:div.date (f/unparse (f/formatters :date)
+                               (tc/from-sql-time (:date entry)))]]
+        [:div.content (:content entry)]]]])))
+
+(defn htmlitize-edit!
+  "make a post html (fill up title content etc)"
+  [entry-title]
+  (let [entry (first (jdbc/query db/pg-db (-> (select :*)
+                                              (from :posts)
+                                              (where [:= :link
+                                                      entry-title])
+                                              sql/format)))]
+    (html5
+     (include-css "/styles/style.css")
+     (include-js "/scripts/contenteditable.js")
+     [:html
+      [:head
+       [:meta {:name "viewport"
+               :content "width=device-width, initial-scale=1.0"}]
+       [:title "solB"]]
+      [:body
+       [:header (layout/navbar)]
+       [:div.blog
+        [:h2#sad {:contenteditable "true"} (:title entry)]
+        [:div.datetagsflex
+         [:div#depressed.tags {:contenteditable "true"}
+          (:tags entry)]
+         [:div.date (f/unparse (f/formatters :date)
+                               (tc/from-sql-time (:date entry)))]]
+        [:div#rip.forward {:contenteditable "true"}
+         (:forward entry)]
+        [:div#unhappy.content {:contenteditable "true"}
+         (:content entry)]]
+       [:form {:enctype "multipart/form-data"
+               :action "/editor"
+               :method "post"
+               :onsubmit "return getContentEditableAll()"}
+        (anti/anti-forgery-field)
+        (form/hidden-field "content")
+        (form/hidden-field "tags")
+        (form/hidden-field "title")
+        (form/hidden-field "forward")
+        (form/hidden-field "id" (:id entry))
+        [:input {:type "submit"}]]]])))
