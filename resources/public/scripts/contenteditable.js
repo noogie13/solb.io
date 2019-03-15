0a66e2aedb7cad347a82ded62c51d74aeab52197
Solomon Bloch <solomonbloch@gmail.com>
2019-03-15 15:24:23 +0900

    added ctrl+s to add code block

--- resources/public/scripts/contenteditable.js
+++ resources/public/scripts/contenteditable.js
@@ -1,6 +1,6 @@
 function getContentEditable(get, call){
-// this function is used to get the stuff inside the contenteditable elements, to be used in the form to edit posts
-        document.getElementById(call).value = document.getElementById(get).innerHTML;
+    // this function is used to get the stuff inside the contenteditable elements, to be used in the form to edit posts
+    document.getElementById(call).value = document.getElementById(get).innerHTML;
 }
 
 function getContentEditableAll() {
@@ -14,6 +14,7 @@ window.onload=function() {
     var contentedit = document.getElementById('unhappy');
     contentedit.addEventListener("keydown", insertTabAtCaret);
     contentedit.addEventListener("keydown", insertNewLineAtCaret);
+    contentedit.addEventListener("keydown", insertCodeBlock);
 }
 
 function insertTabAtCaret(event){
@@ -28,7 +29,24 @@ function insertTabAtCaret(event){
         range.setEndAfter(tabNode);
     }
 }
+function insertCodeBlock(e){
+    if(e.ctrlKey && e.which == 83){
+        e.preventDefault();
+        var range = window.getSelection().getRangeAt(0);
 
+        var pre = document.createElement("pre");
+        var code = document.createElement("code");
+        var example = document.createTextNode("defun");
+
+        pre.appendChild(code);
+        code.appendChild(example);
+
+        range.insertNode(pre);
+
+        range.setStartAfter(pre);
+        range.setEndAfter(pre);
+    }
+}
 function insertNewLineAtCaret(event){
     if(event.keyCode === 13){
         event.preventDefault();
