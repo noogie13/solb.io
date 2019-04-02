945955945a880005748e97b000c6225a5b919215
Solomon Bloch <solomonbloch@gmail.com>
2019-04-02 23:44:20 +0900

    use page-template

--- src/templates/login.clj
+++ src/templates/login.clj
@@ -2,59 +2,39 @@
   (:require
    [clojure.java.io :as io]
    [ring.util.anti-forgery :as anti]
+   [templates.layout :as layout]
    [hiccup.form :as form]
    [hiccup.core :refer :all]
    [hiccup.page :refer [include-css html5]]))
 
 (defn create-page
   []
-  (html5
-   (include-css "/styles/style.css")
-   [:html
-    [:head
-     [:meta {:name "viewport"
-             :content "width=device-width, initial-scale=1.0"}]
-     [:title "solB"]]
-    [:body
-     [:header
-      (templates.layout/navbar)
-      [:h1 "S"
-       [:span {:style "font-size: 26px;"}
-        "OL"] "B"]]
-     [:h1 {:style "color: black;"} "create"]
-     [:div#create.form
-      (form/form-to {:enctype "multipart/form-data"}
-                    [:post "/create"]
-                    (anti/anti-forgery-field)
-                    "username"
-                    (form/text-field "username")
-                    [:br]
-                    "name"
-                    (form/text-field "name")
-                    [:br]
-                    "password"
-                    (form/password-field "password")
-                    (form/submit-button "submit?"))]]]))
+  (layout/page-template
+   [:h1 {:style "color: black;"} "create"]
+   [:div#create.form
+    (form/form-to {:enctype "multipart/form-data"}
+                  [:post "/create"]
+                  (anti/anti-forgery-field)
+                  "username"
+                  (form/text-field "username")
+                  [:br]
+                  "name"
+                  (form/text-field "name")
+                  [:br]
+                  "password"
+                  (form/password-field "password")
+                  (form/submit-button "submit?"))]))
 
 (defn login-page
   []
-  (html5
-   (include-css "/styles/style.css")
-   [:html
-    [:head
-     [:meta {:name "viewport"
-             :content "width=device-width, initial-scale=1.0"}]
-     [:title "solB"]]
-    [:body
-     [:header
-      (templates.layout/navbar)]
-     [:div#login.form
-      (form/form-to {:enctype "multipart/form-data"}
-                    [:post "/login"]
-                    (anti/anti-forgery-field)
-                    "user"
-                    (form/text-field "username")
-                    [:br]
-                    "pass"
-                    (form/password-field "password")
-                    (form/submit-button "submit?"))]]]))
+  (layout/page-template
+   [:div#login.form
+    (form/form-to {:enctype "multipart/form-data"}
+                  [:post "/login"]
+                  (anti/anti-forgery-field)
+                  "username"
+                  (form/text-field "username")
+                  [:br]
+                  "password"
+                  (form/password-field "password")
+                  (form/submit-button "submit?"))]))
