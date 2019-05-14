99abed0d1a20f589675e2583ac38f4e20d4a1448
Solomon Bloch <solomonbloch@gmail.com>
2019-05-14 19:07:15 +0900

    added shortening

--- src/backend/db.clj
+++ src/backend/db.clj
@@ -20,10 +20,16 @@
                                           [:tags "VARCHAR(255)"]
                                           [:forward "VARCHAR(255)"]
                                           [:content "TEXT"]]))
-;; (jdbc/execute! pg-db posts)
-(defn post []
-  (jdbc/execute! pg-db posts))
+;; (jdbc/execute! pg-db shortened)
+
+;; (defn post []
+;;   (jdbc/execute! pg-db posts))
 
 (def users (jdbc/create-table-ddl :users [[:username "VARCHAR(255)"]
                                           [:password "VARCHAR(255)"]
-                                          [:name "VARCHAR(255)"]]))
+                                          [:name "VARCHAR(255)"]
+                                          [:token "VARCHAR(255)"]]))
+
+(def shortened (jdbc/create-table-ddl :shortened [[:id "VARCHAR(255)"]
+                                                  [:type "VARCHAR(255)"]
+                                                  [:data "VARCHAR(255)"]]))
