4e75f2af54e36eef103c7115ccd5667aac451f6d
Solomon Bloch <solomonbloch@gmail.com>
2019-06-02 20:05:12 +0900

    calendar script wrapper

---
+++ src/backend/calendar.clj
@@ -0,0 +1,27 @@
+(ns backend.calendar
+  (:require
+   [clojure.java.shell :refer [sh]]
+   [clojure.java.io :as io]
+   [ring.util.codec :as codec]))
+
+(defonce path "src/backend/")
+
+;; (re-find #"'.*'" blah)
+;; (sh "./calscript.py")
+
+(defn get-ics-string
+  [input-string]
+  (as-> (sh "./calscript.py" input-string :dir path) $
+       (:out $)
+       (re-find #"'(.*)'" $)
+       (nth $ 1)))
+
+(defn get-ics-from-req
+  [req]
+  (let [string (->> (req :params)
+                    (reduce into [])
+                    (apply str)
+                    (get-ics-string))]
+    {:status 200
+     :headers {}
+     :body string}))
