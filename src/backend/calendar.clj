02f25734df70f4d7bf1596f95b87dac48299a1ea
Solomon Bloch <solomonbloch@gmail.com>
2019-06-03 14:47:03 +0900

    maybe this is the problem?

--- src/backend/calendar.clj
+++ src/backend/calendar.clj
@@ -15,7 +15,7 @@
 
 (defn get-ics-from-req
   [req]
-  (let [string (->> (req :query-params)
+  (let [string (->> (req :params)
                     (reduce into [])
                     (apply str)
                     (get-ics-string))]
